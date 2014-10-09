ca4844eea3b8fb508409ef6feba2b7c683a3ff1d
LEVEQUE Franck <franck.leveque2@free.fr>
2014-10-09 12:09:20 +0200

    Created Nuget package Modified assembly version to follow Cloud version Added description to properties Added LICENSE and nuget package in solution files.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MWS")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription(".Net connector to allow access to the movilizer cloud api")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Movilitas")]
 [assembly: AssemblyProduct("MWS")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
